13fe6f8cc78458b3cddee25cb94a367455ee9395
mmassime <56897994+mmassime@users.noreply.github.com>
2021-10-13 17:17:01 +0200

    Ej. 2

--- automata/re_parser.py
+++ automata/re_parser.py
@@ -93,11 +93,66 @@ class REParser(AbstractREParser):
         automaton1: FiniteAutomaton,
         automaton2: FiniteAutomaton,
     ) -> FiniteAutomaton:
-        raise NotImplementedError("This method must be implemented.")
+        self.state_counter=0
+        states = list(automaton1.states + automaton2.states)
+        transitions = list(automaton1.transitions + automaton2.transitions)
+        symbols = automaton1.symbols + automaton2.symbols
+        initial = State(name="q"+str(self.state_counter),is_final=False)
+        states.append(initial)
+        self.state_counter+=1
+        for state in states:
+            if state != initial:
+                for transition in transitions:
+                    if transition.initial_state == state:
+                        transition.initial_state.name = "q"+str(self.state_counter)
+                    if transition.final_state == state:
+                        transition.final_state.name = "q"+str(self.state_counter)
+                state.name = "q"+str(self.state_counter)
+                self.state_counter +=1
+        transitions.append(Transition(initial_state = initial, symbol=None, final_state=automaton1.initial_state))
+        transitions.append(Transition(initial_state = initial, symbol=None, final_state=automaton2.initial_state))
+        final = State(name="f"+str(self.state_counter), is_final=True)
+        self.state_counter+=1
+        states.append(final)
+        for state in states:
+            if state.is_final and state != final:
+                state.is_final = False
+                transitions.append(Transition(initial_state = state, symbol=None, final_state=final))
 
+        return FiniteAutomaton(initial_state=initial,states=states,symbols=symbols,transitions=transitions)
+    
     def _create_automaton_concat(
         self,
         automaton1: FiniteAutomaton,
         automaton2: FiniteAutomaton,
     ) -> FiniteAutomaton:
-        raise NotImplementedError("This method must be implemented.")
+        self.state_counter=0
+        symbols = automaton1.symbols + automaton2.symbols
+        transitions = list(automaton1.transitions + automaton2.transitions)
+        final = State(name="final", is_final=True)
+        for state in automaton1.states:
+            if state.is_final:
+                state.is_final = False
+                transitions.append(Transition(initial_state = state, symbol=None, final_state=automaton2.initial_state))
+        for state in automaton2.states:
+            if state.is_final:
+                state.is_final = False
+                transitions.append(Transition(initial_state = state, symbol=None, final_state=final))
+        states = list(automaton1.states + automaton2.states)
+        states.append(final)
+        initial = State(name="q"+str(self.state_counter),is_final=False)
+        self.state_counter+=1
+        states.append(initial)
+        for state in states:
+            if state != initial:
+                for transition in transitions:
+                    if transition.initial_state == state:
+                        transition.initial_state.name = "q"+str(self.state_counter)
+                    if transition.final_state == state:
+                        transition.final_state.name = "q"+str(self.state_counter)
+                state.name = "q"+str(self.state_counter)
+                self.state_counter +=1
+        final.name="f"+str(self.state_counter)
+        self.state_counter += 1
+        transitions.append(Transition(initial_state = initial, symbol=None, final_state=automaton1.initial_state))
+        return FiniteAutomaton(initial_state=initial,states=states,symbols=symbols,transitions=transitions)
